adffe2b6e110648731dbdac6e5b4a297de8d4893
student-kajal <kajal971126@gmail.com>
2025-04-25 02:42:35 +0530

    fix: use dynamic port for render deployment

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ app.post("/delete-stock", async (req, res) => {
 });
 
 
-const port = 3000;
+const port =  process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`✅ Server running on http://localhost:${port}`);
 });
